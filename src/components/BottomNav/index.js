a08d18ddb22176d045a4506df0eeccfd34354acb
gaoshaoyun <276185876@qq.com>
2020-06-16 16:29:33 +0800

    save lesson1

---
+++ src/components/BottomNav/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class BottomNav extends Component {
+  render() {
+    return (
+      <div>
+        <h3>BottomNav</h3>
+      </div>
+    );
+  }
+}
